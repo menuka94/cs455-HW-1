8d2aa7e148e836c8165815b3950973289e820f0a
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2020-01-23 21:33:44 -0700

    chore: configure log4j

--- src/main/java/cs455/overlay/Node.java
+++ src/main/java/cs455/overlay/Node.java
@@ -1,10 +1,11 @@
 package cs455.overlay;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class Node {
-    private static Logger logger = LoggerFactory.getLogger(Node.class);
+    private static final Logger logger = LogManager.getLogger(Node.class);
     public static void main(String[] args) {
         logger.info("Hello World from log4j");
     }
