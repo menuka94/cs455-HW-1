c231c4e8bbf03a2bedfc98624ab6de3afb04f348
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2020-02-07 15:57:53 -0700

    update NODE_REPORTS_OVERLAY_SETUP_STATUS

--- src/main/java/cs455/overlay/wireformats/NodeReportsOverlaySetupStatus.java
+++ src/main/java/cs455/overlay/wireformats/NodeReportsOverlaySetupStatus.java
@@ -1,8 +1,21 @@
 package cs455.overlay.wireformats;
 
+import cs455.overlay.util.Validator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
 import java.net.Socket;
 
 public class NodeReportsOverlaySetupStatus extends Event {
+    private static final Logger logger = LogManager.getLogger(NodeReportsOverlaySetupStatus.class);
+    private byte messageType;
+    private int successStatus;
+    private byte lengthOfInfoString;
+    private byte[] infoString;
+
+    public NodeReportsOverlaySetupStatus() {
+    }
 
     /**
      * byte: Message type (NODE_REPORTS_OVERLAY_SETUP_STATUS)
@@ -10,14 +23,51 @@ public class NodeReportsOverlaySetupStatus extends Event {
      * byte: Length of following "Information string" field
      * byte[^^]: Information string; ASCII charset
      */
-    public NodeReportsOverlaySetupStatus(byte[] marshalledBytes) {
+    public NodeReportsOverlaySetupStatus(byte[] marshalledBytes) throws IOException {
+        ByteArrayInputStream baInputStream = new ByteArrayInputStream(marshalledBytes);
+        DataInputStream din = new DataInputStream(new BufferedInputStream(baInputStream));
 
+        messageType = din.readByte();
+        Validator.validateEventType(messageType, Protocol.NODE_REPORTS_OVERLAY_SETUP_STATUS, logger);
+
+        successStatus = din.readInt();
+        lengthOfInfoString = din.readByte();
+        infoString = new byte[lengthOfInfoString];
+        din.readFully(infoString, 0, lengthOfInfoString);
+
+        baInputStream.close();
+        din.close();
     }
 
 
     @Override
     public byte[] getBytes() {
-        return new byte[0];
+        byte[] marshalledBytes = null;
+        ByteArrayOutputStream baOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baOutputStream));
+
+        try {
+            dout.writeByte(getType());
+            dout.writeInt(successStatus);
+            dout.writeByte(lengthOfInfoString);
+            dout.write(infoString);
+
+            dout.flush();
+
+            marshalledBytes = baOutputStream.toByteArray();
+
+        } catch (IOException e) {
+            logger.error(e.getStackTrace());
+        } finally {
+            try {
+                baOutputStream.close();
+                dout.close();
+            } catch (IOException e) {
+                logger.error(e.getStackTrace());
+            }
+        }
+
+        return marshalledBytes;
     }
 
     @Override
